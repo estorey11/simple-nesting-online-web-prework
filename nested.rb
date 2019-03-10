1b67f5a78f6808fb8e61a3a53784222a3e0c0049
Eric Storey <estorey@gmail.com>
2019-03-10 20:30:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -82,7 +83,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[]
 end
 
 def changing_alan
